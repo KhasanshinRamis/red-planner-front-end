248d7fcc20a07777bdd2f6ed9982ff001c53be6d
Ramis <khasan07@list.ru>
2025-12-20 17:21:54 +0300

    fix redirect root page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: "/",
+                destination: "/i",
+                permanent: true, // true для 308 редиректа, false для 307
+            },
+        ];
+    },
+};
 
 export default nextConfig;
